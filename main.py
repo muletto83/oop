784d6019d0b11a92611ba6af1c943fb08391dbba
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-17 01:26:26 -0400

    CH2-L1 start

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-def get_soldier_dps(soldier):  
-    return soldier["damage"] * soldier["attacks_per_second"]
-def fight_soldiers(soldier_one, soldier_two):
-    soldier_one_dps = get_soldier_dps(soldier_one)
-    soldier_two_dps = get_soldier_dps(soldier_two)
-    if soldier_one_dps > soldier_two_dps:
-        return "soldier 1 wins"
-    if soldier_two_dps > soldier_one_dps:
-        return "soldier 2 wins"
-    return "both soldiers die"
+'''Create a class called Wall. It should have:
+A property called armor initialized to (initially set to) 10
+A property called height initialized to 5
+Create a class called BatteringRam. It should have:
+A property called damage initialized to 2
+A property called length initialized to 4'''
+
+class Wall:
+    pass
+
+
+class BatteringRam:
+    pass
