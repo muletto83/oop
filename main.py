c324800f1e97876b48bd74e242b4de41d0d41c83
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-17 01:44:04 -0400

    CH2-L3 start

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-'''Complete the fortify() method on the wall class. 
-It should double the current armor property.'''
+'''
+Building walls in Age of Dragons can be expensive,
+the larger and stronger the wall, the more it costs.
+Complete the .get_cost() method on the Wall class. 
+It should return the cost of a wall,
+where the cost is its armor multiplied by its height.
+'''
 
 class Wall:
     armor = 10
     height = 5
 
+    def get_cost(self):
+        pass
+
+    # don't touch below this line
+
     def fortify(self):
+
         self.armor *= 2
