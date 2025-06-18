9156450ba9efe564bfc6c43f8d662519d4c9e26d
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-18 11:32:27 -0400

    CH3-L4 start

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 '''
 Assignment
-Complete the Wizard class's constructor.
+Complete the following methods on the Wizard class:
 
-Set 2 private properties (be sure to include the private __ prefix):
-stamina
-intelligence
-Set 3 public properties:
-name: Use the value passed into the constructor
-health: 100x the value of "stamina"
-mana: 10x the value of "intelligence"
+get_fireballed() should:
+Reduce the fireball_damage by the wizard's __stamina
+Reduce the wizard's health by the resulting fireball_damage
+drink_mana_potion() should:
+Increase the potion_mana by the wizard's __intelligence
+Increase the wizard's mana by the resulting potion_mana
+Both methods operate directly on the instance of the class (self).
+They take one input and return no values explicitly.
 '''
 
 class Wizard:
     def __init__(self, name, stamina, intelligence):
+        self.name = name
         self.__stamina = stamina
         self.__intelligence = intelligence
-        self.name = name
-        self.health = self.__stamina * 100
         self.mana = self.__intelligence * 10
+        self.health = self.__stamina * 100
+
+    # don't touch above this line
+
+    def get_fireballed(self, fireball_damage):
+        pass
+
+    def drink_mana_potion(self, potion_mana):
+        pass
