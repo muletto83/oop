a08f51719669866c6698a5c9b6b0ed72ecb38ee8
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-18 10:31:32 -0400

    CH2-L11 start

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
-'''Assignment
-Complete the Archer class.
-
-Complete the constructor. It should take the following parameters in order and set them as instance properties:
-name
-health
-num_arrows
-Complete the take_hit method. It operates on the current archer instance.
-If the archer has no health, raise the exception: {NAME} is dead where {NAME} is the archer's name.
-Otherwise, remove one health from the current archer.
-Finish the shoot method. It takes an Archer instance as its target input.
-If the shooter has no arrows left, raise an exception {NAME} can't shoot where {NAME} is the shooter's name.
-Otherwise, remove an arrow from the shooter.
-Print {1} shoots {2} where {1} is the shooter's name and {2} is the name of the targeted archer.
-Call the target's take_hit() method.
+'''
+Assignment
+Some lazy class variable code written by another dev team 
+at Age of Dragons Studios is causing bugs in our team's Dragon class.
+In the main() function (that our team isn't responsible for) the line:
+Dragon.element = "fire"
+should not affect our existing Dragon instances!
+The Dragon class should be safe to use in other parts of the codebase,
+even if silly developers are out there changing class-level variables.
+Fix the Dragon class.
+Remove the element class variable.
+Use an instance variable for element, and allow it to be set in the constructor.
 '''
 
-class Archer:
-    def __init__(self, name, health, num_arrows):
-        self.name = name
-        self.health = health
-        self.num_arrows = num_arrows
+class Dragon:
+    element = "ice"
 
-    def take_hit(self):
-        self.health -= 1 # reduce health by 1
-        if self.health < 0:
-            raise Exception(f"{self.name} is dead")
+    def __init__(self, element):
+        return
 
-    def shoot(self, target):
-        if self.num_arrows <= 0:
-            raise Exception(f"{self.name} can't shoot")
-        print(f"{self.name} shoots {target.name}")
-        self.num_arrows -= 1
-        target.take_hit()  # call the target's take_hit method
+    def get_breath_damage(self):
+        if self.element == "fire":
+            return 300
+        if self.element == "ice":
+            return 150
+        return 0
 
-    # don't touch below this line
 
-    def get_status(self):
-        return self.name, self.health, self.num_arrows
+# don't touch below this line
 
-    def print_status(self):
-        print(f"{self.name} has {self.health} health and {self.num_arrows} arrows")
+
+def main():
+    first_dragon = Dragon("fire")
+    print(
+        f"{first_dragon.element} dragon does {first_dragon.get_breath_damage()} damage"
+    )
+
+    second_dragon = Dragon("ice")
+    Dragon.element = "fire"
+    print(
+        f"{second_dragon.element} dragon does {second_dragon.get_breath_damage()} damage"
+    )
+
+
+main()
