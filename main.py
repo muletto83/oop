66cc579623771b76cca773022829d74f1c66dbce
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-18 21:59:45 -0400

    CH3-L8 started

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
 '''
 Assignment
-Complete the cast_fireball method:
-If there isn't enough mana to cast a fireball
-(see fireball_cost at the top of the file),
-raise an Exception with the message 
-____ cannot cast fireball,
-where ____ is the wizard's name.
-If the wizard has enough mana,
-reduce their mana by the fireball_cost 
-and call get_fireballed on the target wizard
-with the given fireball_damage.
-Complete the is_alive method. 
-It should return True 
-if the wizard's health is greater than 0 and False otherwise.
+Complete the BankAccount class.
+
+Complete the constructor
+Set __account_number to account_number
+Set __balance to initial_balance
+Complete the public getters
+Complete the get_account_number method to get the value of the private variable __account_number and return it.
+Complete the get_balance method to get the value of the private variable __balance and return it.
+Complete the deposit method
+It should accept an amount as input and add it to the account balance.
+If the deposit amount isn't positive, it should raise a ValueError exception with the message cannot deposit zero or negative funds. Otherwise, it should add the amount to the balance.
+Complete the withdraw method
+It should accept an amount and check if there is enough money in the account for the withdrawal.
+If the withdrawal amount isn't positive, it should raise a ValueError exception with the message cannot withdraw zero or negative funds.
+Then, if there are not enough funds it should raise a ValueError exception with the message insufficient funds.
+Otherwise, it should deduct the amount from the balance
 '''
 
-class Wizard:
-    def __init__(self, name, stamina, intelligence):
-        self.name = name
-        self.__stamina = stamina
-        self.__intelligence = intelligence
-        self.mana = self.__intelligence * 10
-        self.health = self.__stamina * 100
+class BankAccount:
+    def __init__(self, account_number, initial_balance):
+        pass
 
-    def cast_fireball(self, target, fireball_cost, fireball_damage):
-        if self.mana < fireball_cost:
-            raise Exception(f"{self.name} cannot cast fireball")
-        self.mana -= fireball_cost
-        target.get_fireballed(fireball_damage)
+    def get_account_number(self):
+        pass
 
-    def is_alive(self):
-        return self.health > 0
+    def get_balance(self):
+        pass
 
-    def get_fireballed(self, fireball_damage):
-        fireball_damage -= self.__stamina
-        self.health -= fireball_damage
+    def deposit(self, amount):
+        pass
 
-    def drink_mana_potion(self, potion_mana):
-        potion_mana += self.__intelligence
-        self.mana += potion_mana
+    def withdraw(self, amount):
+        pass
