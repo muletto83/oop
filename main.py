165be2c60bcb2cf9974cd9924e7e91b42153c488
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-18 23:06:42 -0400

    CH4-L4 start

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 '''
 Assignment
-We don't want our coworkers at Age of Dragons Studios™ to have to worry about how Humans move. 
-We'll abstract that away from them,
-by encapsulating the private __pos_x, __pos_y, and __speed variables behind some simple methods.
+Complete all the missing methods:
 
-Complete the following methods in the Human class:
-
-move_right(): Adds the human's speed to its x position.
-move_left(): Subtracts the human's speed from its x position.
-move_up(): Adds the human's speed to its y position.
-move_down(): Subtracts the human's speed from its y position.
-get_position(): Returns the x position and y position as a tuple.
+Complete the private helper methods, which are intended to be used by the other four sprinting methods.
+__raise_if_cannot_sprint(): Raise the exception: not enough stamina to sprint if the human is out of stamina.
+__use_sprint_stamina(): Remove one stamina from the human.
+For each of the sprint methods:
+Raise an error if there isn't enough stamina to sprint (use __raise_if_cannot_sprint()).
+Use the stamina needed to sprint (use __use_sprint_stamina())
+Move twice in the direction of the sprint.
 '''
 
 class Human:
-    def __init__(self, pos_x, pos_y, speed):
-        self.__pos_x = pos_x
-        self.__pos_y = pos_y
-        self.__speed = speed
+    def sprint_right(self):
+        pass
+
+    def sprint_left(self):
+        pass
+
+    def sprint_up(self):
+        pass
+
+    def sprint_down(self):
+        pass
+
+    def __raise_if_cannot_sprint(self):
+        pass
+
+    def __use_sprint_stamina(self):
+        pass
+
+    # don't touch below this line
 
     def move_right(self):
         self.__pos_x += self.__speed
@@ -32,4 +45,10 @@ class Human:
         self.__pos_y -= self.__speed
 
     def get_position(self):
-        return (self.__pos_x, self.__pos_y)
+        return self.__pos_x, self.__pos_y
+
+    def __init__(self, pos_x, pos_y, speed, stamina):
+        self.__pos_x = pos_x
+        self.__pos_y = pos_y
+        self.__speed = speed
+        self.__stamina = stamina
