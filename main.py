4ab4439b76bcbd5010c4979fb0bf96b8c3cea539
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-19 23:18:58 -0400

    CH5-L8 start

--- main.py
+++ main.py
@@ -1,52 +1,38 @@
 '''
 Assignment
-Let's add a new game unit: Crossbowman.
-A crossbowman is always an archer, 
-but not all archers are crossbowmen.
-Crossbowmen have several arrows,
-but they have an additional method: triple_shot().
+Ensure the following requirements
+from the game designers are completed:
 
-Complete the use_arrows method on the Archer class.
-It should remove num arrows, 
-but if there aren't enough arrows to remove, 
-it should raise a not enough arrows exception instead.
-Complete the Crossbowman class.
-Its constructor should call its parent's constructor.
-Its triple_shot method should:
-Use 3 arrows
-Return the string TARGET was shot by 3 crossbow bolts where TARGET is the name of the Human that was shot (any Human can be a target).
+Archer should inherit from Hero.
+Archer should set up the hero's name and health.
+Set a private "number of arrows" variable that 
+can be set by the third parameter to the constructor.
+Complete the shoot method. It takes a target hero as input.
+If there are no arrows left, raise a not enough arrows exception.
+Otherwise, 
+remove an arrow and deal 10 damage to the target hero.
 '''
-class Human: # Base class for all humans
-    # Constructor to initialize the name of the human
-    def __init__(self, name): # Constructor with name parameter
-        self.__name = name # private attribute
+class Hero:
+    def __init__(self, name, health):
+        self.__name = name
+        self.__health = health
 
     def get_name(self):
-        return self.__name # public method to access private attribute
+        return self.__name
+
+    def get_health(self):
+        return self.__health
+
+    def take_damage(self, damage):
+        self.__health -= damage
 
 
-## don't touch above this line
+# don't touch above this line
 
 
-class Archer(Human): # Inherits from Human
-    def __init__(self, name, num_arrows): # Constructor with name and number of arrows
-        super().__init__(name) # Call the parent constructor to set the name
-        self.__num_arrows = num_arrows # Private attribute for number of arrows
+class Archer:
+    def __init__(self, name, health, num_arrows):
+        pass
 
-    def get_num_arrows(self): # Public method to get the number of arrows
-        return self.__num_arrows # public method to access private attribute
-
-    def use_arrows(self, num): # Method to use a certain number of arrows
-        if num > self.__num_arrows: # Check if there are enough arrows
-            raise Exception("not enough arrows") # Raise an exception if not enough arrows
-        self.__num_arrows -= num # Decrease the number of arrows by num
-
-
-class Crossbowman(Archer): # Inherits from Archer
-    def __init__(self, name, num_arrows): # Constructor with name and number of arrows
-        super().__init__(name, num_arrows) # Call the parent constructor to set the name and arrows
-
-    def triple_shot(self, target): # Method to perform a triple shot
-        self.use_arrows(3) # Use 3 arrows
-        # Return a formatted string indicating the target was shot by 3 crossbow bolts
-        return f"{target.get_name()} was shot by 3 crossbow bolts" 
+    def shoot(self, target):
+        pass
