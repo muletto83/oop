29cfc502c1d14566dcfb67975ed0f6786709fa58
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-20 18:30:32 -0400

    CH5-L9 start

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 '''
 Assignment
-Ensure the following requirements
-from the game designers are completed:
+Complete the Wizard class.
 
-Archer should inherit from Hero.
-Archer should set up the hero's name and health.
-Set a private "number of arrows" variable that 
-can be set by the third parameter to the constructor.
-Complete the shoot method. It takes a target hero as input.
-If there are no arrows left, raise a not enough arrows exception.
-Otherwise, 
-remove an arrow and deal 10 damage to the target hero.
+Wizard should inherit from Hero.
+Wizard should set up the hero's name and health.
+Set a private mana variable that
+can be passed in as a third parameter to the constructor.
+Create a cast method that takes a target hero as input.
+If there is less than 25 mana left,
+raise a not enough mana exception.
+Otherwise, remove 25 mana from the wizard 
+and deal 25 damage to the target hero.
 '''
 class Hero:
     def __init__(self, name, health):
@@ -27,16 +27,24 @@ class Hero:
         self.__health -= damage
 
 
-# don't touch above this line
-
-
 class Archer(Hero):
     def __init__(self, name, health, num_arrows):
         super().__init__(name, health)
         self.__num_arrows = num_arrows
-        
+
     def shoot(self, target):
         if self.__num_arrows <= 0:
             raise Exception("not enough arrows")
         self.__num_arrows -= 1
         target.take_damage(10)
+
+
+# don't touch above this line
+
+
+class Wizard(Hero):
+    def __init__(self, name, health, mana):
+        pass
+
+    def cast(self, target):
+        pass
