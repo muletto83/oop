a4782324973a66e358d468ba10fcfc269931e226
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-19 22:34:16 -0400

    CH5-L5 start

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 '''
 Assignment
-In Age of Dragons, all the archers are humans, 
-but not all humans are necessarily archers.
-All humans have a name,
-but only archers have a __num_arrows property.
+Let's add a new game unit: Crossbowman.
+A crossbowman is always an archer, 
+but not all archers are crossbowmen.
+Crossbowmen have several arrows,
+but they have an additional method: triple_shot().
 
-Complete the Archer class. It should inherit the Human class.
-
-Its constructor should:
-Call the parent constructor
-Set the private __num_arrows property based
-on the constructor parameter
-Its get_num_arrows() method should return the number of arrows 
-the archer has.
+Complete the use_arrows method on the Archer class.
+It should remove num arrows, 
+but if there aren't enough arrows to remove, 
+it should raise a not enough arrows exception instead.
+Complete the Crossbowman class.
+Its constructor should call its parent's constructor.
+Its triple_shot method should:
+Use 3 arrows
+Return the string TARGET was shot by 3 crossbow bolts where TARGET is the name of the Human that was shot (any Human can be a target).
 '''
 class Human:
     def __init__(self, name):
@@ -27,8 +29,19 @@ class Human:
 
 class Archer(Human):
     def __init__(self, name, num_arrows):
-        super().__init__(name)  # Call the parent constructor
-        self.__num_arrows = num_arrows # Set the private __num_arrows property
+        super().__init__(name)
+        self.__num_arrows = num_arrows
 
     def get_num_arrows(self):
-        return self.__num_arrows  # Return the number of arrows the archer has
+        return self.__num_arrows
+
+    def use_arrows(self, num):
+        pass
+
+
+class Crossbowman(Archer):
+    def __init__(self, name, num_arrows):
+        pass
+
+    def triple_shot(self, target):
+        pass
