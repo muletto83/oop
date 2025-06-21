f205c18322c711096aaad838518d9737f618b60f
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-21 14:54:40 -0400

    CH6-L2 start

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 '''
 Assignment
-Let's build some hit-box logic for our game, starting with a simple Rectangle.
-Complete the __init__() method. Configure the class to have properties matching the variables passed into the constructor in this order:
+We changed the coordinates themselves to be private by adding two underscores to them. We now need to write getter methods to access them.
 
-x1
-y1
-x2
-y2
+Complete the following methods:
 
+get_left_x(): Returns the leftmost (smallest) x value
+get_right_x(): Returns the rightmost (largest) x value
+get_top_y(): Returns the topmost (largest) y value
+get_bottom_y(): Returns the bottom-most (smallest) y value
+Remember that we're working with a standard Cartesian plane.
+
+We will explain the __repr__ method later, don't worry too much about it.
+
+Tips
+You may find Python's built-in min and max functions useful.
 '''
 class Rectangle:
     def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def get_left_x(self):
+        pass
+
+    def get_right_x(self):
+        pass
+
+    def get_top_y(self):
+        pass
+
+    def get_bottom_y(self):
+        pass
+
+    # don't touch below this line
+
+    def __repr__(self):
+        return f"Rectangle({self.__x1}, {self.__y1}, {self.__x2}, {self.__y2})"
